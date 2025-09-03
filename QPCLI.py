db9255a12033df4207c88190efbfac640107d4e2
Nathan <nathan@mcminncs.net>
2025-09-03 13:27:04 -0400

    Update QPCLI.py

--- QPCLI.py
+++ QPCLI.py
@@ -6,7 +6,6 @@ import json
 from datetime import datetime 
 import csv
 import pandas as pd
-
 #print("QuickPod CLI Bugfix 1.1.2.")
 #print("Copyright (C) 2025 QuickPod. All Rights Reserved")
 #Thank you for choosing QuickPod!
